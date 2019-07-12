2717477b290dc537897714ac601dbf2664c6ad3d
hongwei.liu <lhw_tyut@163.com>
2019-07-12 14:42:48 +0800

    + setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "test setup",
+    version = "0.0.1",
+    packages = find_packages(),
+
+    description = "egg test demo",
+    long_description = "test python setup",
+    author = "lhw",
+    author_email = "lhw_tyut@163.com",
+
+    license = "GPL",
+    keywords = ("test_setup", "egg"),
+    platforms = "Independant",
+    url = "https://blog.csdn.net/qq_37287621",
+    entry_points = {
+        'console_scripts': [
+            'test_setup = test.helloworld:main'
+        ]
+    }
+)
