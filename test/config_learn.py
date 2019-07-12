3b030deb1ad44c95619a33a7d26bb37d175849c2
hongwei.liu <lhw_tyut@163.com>
2019-07-12 17:15:57 +0800

    + 修改

--- test/config_learn.py
+++ test/config_learn.py
@@ -1,10 +1,12 @@
 from configparser import ConfigParser
+import pkg_resources
+
 
 def config():
     cp1 = ConfigParser()
     cp2 = ConfigParser()
-    cp1.read("test/config_file/mysql1.cfg")
-    cp2.read("test/config_file/mysql2.ini")
+    cp1.read(pkg_resources.resource_filename("test", "config_file/mysql1.cfg"))
+    cp2.read(pkg_resources.resource_filename("test", "config_file/mysql2.ini"))
 
     cp1.get("mysql", "host")
     cp2.get("mysql", "host")
