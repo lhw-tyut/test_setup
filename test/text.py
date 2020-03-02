578bb7fcd0313d6d1fd59dd58f007f49afb1991d
hongwei.liu <hongwei.liu@capitalonline.net>
2020-03-02 15:35:30 +0800

    test auto

--- test/text.py
+++ test/text.py
@@ -1,6 +1,10 @@
-with open('ipmi_ips', 'r') as fp:
+a = '10.100.100.'
+count = 6
+with open('host_ip', 'r') as fp:
     ips = fp.readlines()
-    for ip in ips:
-        temp = ip.split('\n')[0]
-        res = temp.split()
-        print(res)
+
+for i in range(count):
+    ip1 = ips[i][:-1]
+    ip2 = a + str(181 + i)
+    ip3 = '10.10.10.' + str(1 + i)
+    print('%s %s %s' %(ip1, ip2, ip3))
